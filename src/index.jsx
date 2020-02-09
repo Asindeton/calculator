1d9e5899d9d6f442ccc1301841736d336f2491cc
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2020-02-09 18:57:15 +0300

    feat: start calculator proj, add webpack config

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+/* import { hot } from 'react-hot-loader'; */
+import img from '../public/header.jpg';
+
+import './index.scss';
+
+import '@babel/polyfill';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="red-text">Hello Ann</div>
+      <img src={img} alt="img" />
+    </Fragment>
+  );
+}
+
+// const AppWithHot = hot(module)(App);
+
+const mountNode = document.getElementById('app');
+ReactDOM.render(<App name="Jane" />, mountNode);
